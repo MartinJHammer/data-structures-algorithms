4637e988365a8511048e548c7131ac502385b20e
Martin <martin_hammer@live.dk>
2022-10-16 16:58:55 +0200

    feat: menu

--- libs/app/app-routing.module.ts
+++ libs/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'hash-table',
+    loadChildren: () => import('../data-structures/hash-table/hash-table.module').then(m => m.HashTableModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- libs/app/app.module.ts
+++ libs/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
@@ -16,7 +18,9 @@ export const basic = [
 
 export const material = [
   MatToolbarModule,
-  MatIconModule
+  MatIconModule,
+  MatMenuModule,
+  MatButtonModule,
 ];
 
 @NgModule({
