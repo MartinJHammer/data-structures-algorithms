d2769724c216e8c8c7a2287cc1458042d3ea654c
Martin <martin_hammer@live.dk>
2022-10-16 15:59:47 +0200

    test: initial jest test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,23 @@
+import { AppComponent } from './app.component';
+import {
+    async,
+    ComponentFixture,
+    TestBed
+} from '@angular/core/testing';
+
 describe('App Component', () => {
-    it('Test', () => {
-        expect(true).toBe(true);
+    let component: AppComponent;
+    let fixture: ComponentFixture<AppComponent>;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [AppComponent]
+        });
+        fixture = TestBed.createComponent(AppComponent);
+        component = fixture.componentInstance;
+    }));
+
+    test('should exist', () => {
+        expect(component).toBeDefined();
     });
-})
+});
