99d6f14ca8878314675a7c5b0435e6db6731e5e7
Martin <martin_hammer@live.dk>
2022-10-16 16:08:49 +0200

    feat: add toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+const basic = [
+  BrowserModule,
+  AppRoutingModule,
+  BrowserAnimationsModule
+];
+
+const material = [
+  MatToolbarModule,
+  MatIconModule
+];
 
 @NgModule({
+  imports: [
+    ...basic,
+    ...material
+  ],
   declarations: [
     AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
